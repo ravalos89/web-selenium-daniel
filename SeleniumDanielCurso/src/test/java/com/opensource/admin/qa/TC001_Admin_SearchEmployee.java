3eda8889cf070fcb69724f0ea825a037762a7dd8
Ricardo Avalos <ricardo.avalos@automationsensei.com>
2022-01-24 18:22:21 -0600

    commit

--- SeleniumDanielCurso/src/test/java/com/opensource/admin/qa/TC001_Admin_SearchEmployee.java
+++ SeleniumDanielCurso/src/test/java/com/opensource/admin/qa/TC001_Admin_SearchEmployee.java
@@ -1,7 +1,12 @@
 package com.opensource.admin.qa;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
@@ -16,5 +21,23 @@ public class TC001_Admin_SearchEmployee {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		driver.manage().window().maximize();
+		
+		// Step 2 
+		Reporter.log("Enter Username, Password and click Login");
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		// IMPLICIT WAIT
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		// Step 3
+		Reporter.log("Validate that you have logged in successfully");
+		// EXPLICIT WAIT
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("welcome")));
+		
+		// Step 4
+		
+		
 	}
 }
