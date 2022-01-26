133b1001f67d8423729a76ac9dbc62fee2e39e72
Ricardo Avalos <ricardo.avalos@automationsensei.com>
2022-01-26 17:59:58 -0600

    script

---
+++ SeleniumDanielCurso/src/main/java/com/opensource/base/GlobalVariables.java
@@ -0,0 +1,9 @@
+package com.opensource.base;
+
+public class GlobalVariables {
+	
+	public static String QA_URL = "https://opensource-demo.orangehrmlive.com/";
+	public static String NAME_CHROME_DRIVER = "webdriver.chrome.driver";
+	public static String PATH_CHROME_DRIVER = "./src/test/resources/drivers/chrome/chromedriver";
+
+}
--- SeleniumDanielCurso/src/test/java/com/opensource/admin/qa/TC001_Admin_SearchEmployee.java
+++ SeleniumDanielCurso/src/test/java/com/opensource/admin/qa/TC001_Admin_SearchEmployee.java
@@ -7,25 +7,50 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.opensource.base.GlobalVariables;
 
 public class TC001_Admin_SearchEmployee {
+	
+	private static String MAIN_USER = "Admin";
+	private static String MAIN_PWD = "admin123";
 
 	@Test
 	public void tc001() {
+		
+		SoftAssert softAssert = new SoftAssert();
 
 		// Step 1
 		Reporter.log("Open Browser \"OrangeHRM\" web page");
-		System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chrome/chromedriver");
+		System.setProperty(GlobalVariables.NAME_CHROME_DRIVER, GlobalVariables.PATH_CHROME_DRIVER);
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.get(GlobalVariables.QA_URL);
 		driver.manage().window().maximize();
 		
 		// Step 2 
 		Reporter.log("Enter Username, Password and click Login");
-		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("txtUsername")).sendKeys(MAIN_USER);
+		driver.findElement(By.id("txtPassword")).sendKeys(MAIN_PWD);
+		boolean isBtnLogindisplayed = driver.findElement(By.id("btnLogin")).isDisplayed();
+		softAssert.assertTrue(isBtnLogindisplayed); // SOFTASSERTION #1
+		
+//		// Suma
+//		int num1 = 1;
+//		int num2 = 1;
+//		int suma = 3;
+//		boolean isEqual;
+//		if((num1+num2)==suma) {
+//			isEqual=true;
+//		}else {
+//			isEqual=false;
+//		}
+		
+//		softAssert.assertTrue(isEqual);//Softassertion #3
+		
 		driver.findElement(By.id("btnLogin")).click();
 		// IMPLICIT WAIT
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -37,7 +62,37 @@ public class TC001_Admin_SearchEmployee {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("welcome")));
 		
 		// Step 4
+		Reporter.log("Click Admin - Go to the admin page");
+		driver.findElement(By.id("menu_admin_viewAdminModule")).click();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		
+		//Step 5
+		Reporter.log("Search username in field \"Username\"");
+		driver.findElement(By.id("searchSystemUser_userName")).sendKeys(MAIN_USER);
+		
+		// Step 6
+		Reporter.log("Click Search");
+		driver.findElement(By.id("searchBtn")).click();
+		
+		// Step 7 
+		Reporter.log("Verify username exist in table");
+		String actualValue = driver.findElement(By.xpath("//tbody/tr[1]/td[2]")).getText();
+		Assert.assertEquals(actualValue, MAIN_USER); // HARD ASSERTION
+		
+		softAssert.assertEquals(actualValue, MAIN_USER); // SOFT ASSERTION #2
+		
+		// Step 8
+		Reporter.log("Log out");
+		driver.findElement(By.id("welcome")).click();
+		driver.findElement(By.xpath("//a[@href=\"/index.php/auth/logout\"]")).click();
+		
+		// Step 9
+		Reporter.log("Close Browser");
+		driver.close();
+		
+		softAssert.assertAll(); // Sirve para indicar que se corran los assertions previos
+		
+
 		
 	}
 }
