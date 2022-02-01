e3650550287196cca275b52d9043cfd6b02047e2
Ricardo Avalos <ricardo.avalos@automationsensei.com>
2022-01-31 20:50:49 -0600

    ddd

--- SeleniumDanielCurso/src/main/java/com/opensource/poc/Admin.java
+++ SeleniumDanielCurso/src/main/java/com/opensource/poc/Admin.java
@@ -1,5 +1,30 @@
 package com.opensource.poc;
 
-public class Admin {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.opensource.base.Base;
+
+public class Admin extends Base {
+
+	public Admin(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+	// Objects
+	By txtSearchUser = By.id("searchSystemUser_userName");
+	By btnSearch = By.id("searchBtn");
+	By tblUsers = By.xpath("//tbody/tr[1]/td[2]");
+
+	/*
+	 * Search user
+	 */
+	public void searchUser(String user) {
+		reporterLog("Search username in field Username");
+		type(txtSearchUser, user);
+		reporterLog("Click Search");
+		click(btnSearch);
+	}
 
 }
