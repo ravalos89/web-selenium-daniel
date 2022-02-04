dac137995dfbfaa5cf6ca73ca9f89adf6b7195b4
Ricardo Avalos <ricardo.avalos@automationsensei.com>
2022-02-04 17:34:16 -0600

    upload methods

--- SeleniumDanielCurso/src/main/java/com/opensource/base/Base.java
+++ SeleniumDanielCurso/src/main/java/com/opensource/base/Base.java
@@ -1,7 +1,18 @@
 package com.opensource.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -125,6 +136,57 @@ public class Base {
 			return softAssert;
 		}catch(AssertionError e) {
 			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * Get Data from JSON file (Directly)
+	 * 
+	 * @author Ricardo Avalos
+	 * @param jsonFile, jsonKey
+	 * @return jsonValue
+	 * @throws FileNotFoundException
+	 */
+	public String getJSONData(String jsonFileObj, String jsonKey) throws FileNotFoundException {
+		try {
+
+			// JSON Data
+			InputStream inputStream = new FileInputStream(GlobalVariables.PATH_JSON_DATA + jsonFileObj + ".json");
+			JSONObject jsonObject = new JSONObject(new JSONTokener(inputStream));
+
+			// Get Data
+			String jsonValue = (String) jsonObject.get(jsonKey);
+			return jsonValue;
+
+		} catch (FileNotFoundException e) {
+			Assert.fail("JSON file is not found");
+			return null;
+		}
+	}
+	
+	/*
+	 * Get Value from Excel
+	 * @author Ricardo Avalos 
+	 * @date 02/18/2019
+	 */
+	public String getCellData(String excelName, int row, int column) {
+		try {
+			// Reading Data
+			FileInputStream fis = new FileInputStream(GlobalVariables.PATH_EXCEL_DATA+excelName+".xlsx");
+			// Constructs an XSSFWorkbook object
+			@SuppressWarnings("resource")
+			Workbook wb = new XSSFWorkbook(fis);
+			Sheet sheet = wb.getSheetAt(0);
+			Row rowObj = sheet.getRow(row);
+			Cell cell = rowObj.getCell(column);
+			String value = cell.getStringCellValue();
+			return value;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e1) {
+			e1.printStackTrace();
 			return null;
 		}
 	}
--- SeleniumDanielCurso/src/main/java/com/opensource/base/GlobalVariables.java
+++ SeleniumDanielCurso/src/main/java/com/opensource/base/GlobalVariables.java
@@ -5,5 +5,7 @@ public class GlobalVariables {
 	public static String QA_URL = "https://opensource-demo.orangehrmlive.com/";
 	public static String NAME_CHROME_DRIVER = "webdriver.chrome.driver";
 	public static String PATH_CHROME_DRIVER = "./src/test/resources/drivers/chrome/chromedriver";
+	public static final String PATH_EXCEL_DATA = "./src/test/resources/testdata/excel/";
+	public static final String PATH_JSON_DATA = "./src/test/resources/testdata/json/";
 
 }
